4e9395513e31f49ee696df29d21ab3cd18b40f25
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-07-21 17:05:28 -0500

    starting firestore functionality

--- real-time-chat/src/main.js
+++ real-time-chat/src/main.js
@@ -3,6 +3,27 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import firebase from "firebase";
+
+// Required for side-effects
+require("firebase/firestore");
+var firebaseConfig = {
+  apiKey: "AIzaSyC2j4yMZ__UGjs8AvEA4GD2DS9VQPOEteo",
+  authDomain: "fitz-chat.firebaseapp.com",
+  databaseURL: "https://fitz-chat.firebaseio.com",
+  projectId: "fitz-chat",
+  storageBucket: "",
+  messagingSenderId: "875055938594",
+  appId: "1:875055938594:web:05dd3c215d5133b6"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var db = firebase.firestore();
+
+// access the variable throughout
+window.db = db;
+
 Vue.config.productionTip = false;
 
 new Vue({
